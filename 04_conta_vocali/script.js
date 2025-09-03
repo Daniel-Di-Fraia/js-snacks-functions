2d3ec5542aced5dbd28e4d8ad00e3c1b18656f35
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-03 16:15:29 +0200

    finalizzazione 04_conta_vocali + miglioramento con vocali tramite array

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,32 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocalsNumber(parola){
+    let vocale = 0;
+    const vocali = ["a","e","i","o","u"]
+    for(let i = 0; i < parola.length; i++){
+        let controllo = parola.charAt(i);
+    for(let i = 0; i < vocali.length; i++){
+        const arrayCheck = vocali[i];
+        // if(
+        //     controllo === "a" ||
+        //     controllo === "e" ||
+        //     controllo === "i" ||
+        //     controllo === "o" ||
+        //     controllo === "u"
+        // ){
+        //     vocale++;
+        // }
+        if(controllo === arrayCheck){
+            vocale++;
+        }
+    }
+    }
+    return vocale;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log("le vocali nella parola sono:" , vocalsNumber(word));
 
 
 
