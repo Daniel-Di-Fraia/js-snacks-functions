f0d9c437534f174b7cc638e4e069a50ecda91664
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-03 15:02:56 +0200

    finalizzazione 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -3,11 +3,26 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
-// Dichiara la funzione qui.
 
+// Dichiara la funzione qui.
+function elementfirstLetter (array){
+    //creazione nuovo array per prima lettera elementi
+    const newArray = [];
+    //scorriamo l'array
+        for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        //creiamo una variabile che prendera come valore il primo carattere degli elementi
+        const primocarattere = num.charAt(0);
+        //pushamo all interno del nuovo array le prime lettere
+        newArray.push(primocarattere);
+        }
+        return newArray;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(elementfirstLetter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
